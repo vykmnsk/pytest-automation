d3b27d094c3dab3cbfee89c3d184ce924f8c7844
Yuri Klind <A143156@agl.com.au>
2018-09-05 15:04:05 +1000

    Add batch script to set a test env vars

---
+++ _testenv/dev-local.cmd
@@ -0,0 +1,11 @@
+set PYTEST_ADDOPTS=--env=dev-local  ^
+--apicode=dummy ^
+--cosmoskey=dummy ^
+--storekey=dummy ^
+--dbuid=dummy ^
+--dbpwd=dummy ^
+--ftpuid=anonymous ^
+--ftppwd=anonymous
+
+set HTTPS_PROXY=
+set HTTP_PROXY=
